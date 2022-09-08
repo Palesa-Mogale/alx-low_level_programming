17f5607768eafe40e4a093f9bf03ea3d385869bf
Palesa Mogale <pmogale88@gmail.com>
2022-09-08 16:51:22 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
