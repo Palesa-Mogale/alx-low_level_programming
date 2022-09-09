017ca48378045097267d1dca04adc395b28bdf12
Palesa Mogale <pmogale88@gmail.com>
2022-09-08 17:21:02 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-printf(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+Write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
