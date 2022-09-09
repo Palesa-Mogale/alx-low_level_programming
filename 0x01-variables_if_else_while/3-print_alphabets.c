c10bf275b4b53e973f2bec738b1978a5036d5c8a
Palesa Mogale <pmogale88@gmail.com>
2022-09-09 16:39:59 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,17 @@
  * Return: 0 (success)
  */
 int main(void)
+
 {
 char lower_case;
 char upper_case;
 
-for (lower_case = 'a'; lower_case <='z'; lower_case++)
+for (lower_case = 'a' ; lower_case <='z' ; lower_case++)
 {
 putchar(lower_case);
 }
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+for (upper_case = 'A' ; upper_case <= 'Z' ; upper_case++)
 {
 putchar(upper_case);
 }
