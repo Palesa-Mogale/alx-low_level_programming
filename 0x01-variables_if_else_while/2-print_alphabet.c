ab00bb797edaef108201a33cccdca6111a4a2e9e
Palesa Mogale <pmogale88@gmail.com>
2022-09-09 16:01:37 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,14 @@
  * Return: 0 (success)
  */
 int main(void)
+
 {
 char c;
 
-for(c='a'; c <='z'; c++)
-{
-	putchar(c);
-}
+for (c ='a'; c <='z'; c++)
+
+putchar(c);
+
 putchar('n\');
 return (0);
 }
