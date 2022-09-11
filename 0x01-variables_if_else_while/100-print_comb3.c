86db9ec8a0893a78fc240b907b005c42709966be
Palesa Mogale <pmogale88@gmail.com>
2022-09-11 08:30:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,57 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
- * main - entry point
  *
- * Return: 0 (success)
- */
-int main(void)
-{
-	int i,j;
+ *  * main - print single digit numbers starting from 0
+ *
+ *   *
+ *
+ *    * Return: returns zero at the end
+ *
+ *     */
 
-	for (i = 0; i<= 9; i++)
-	{
-		for(j = 1; j <= 9; j++)
-		{
-			if (j>i)
-			{
-				putchar(i + '0')
-					putchar(j + '0')
-					
-					if(i != 8)
+int main(void)
+
+{
+
+		int i, j;
+
+
+
+			for (i = 0; i <= 9; i++)
+
 					{
-						putchar(',');
-						putchar(' ');
-					}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+
+								for (j = 1; j <= 9; j++)
+
+											{
+
+															if (j > i)
+
+																			{
+
+																								putchar(i + '0');
+
+																												putchar(j + '0');
+
+																																if (i != 8)
+
+																																					{
+
+																																											putchar(',');
+
+																																																putchar(' ');
+
+																																																				}
+
+																																			}
+
+																	}
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+
