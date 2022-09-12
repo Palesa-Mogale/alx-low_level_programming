eeb99813d8b241d52fc9b7b27d49a18907eccf96
Palesa Mogale <pmogale88@gmail.com>
2022-09-12 13:21:43 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,18 @@ int main(void)
 	int c = 0;
 	int f_d;
 	int l_d;
-        while (c <= 99)
+       
+       	while (c <= 99)
 	{
 		f_d = (c / 10 + '0');
 		l_d = (c % 10 + '0');
+		
 		if (f_d < l_d)
 		{
 			putchar(f_d);
 			putchar(l_d);
-                        if (c != 89)
+                        
+			if (c != 89)
 			{
 				putchar(',');
 				putchar(' ');
