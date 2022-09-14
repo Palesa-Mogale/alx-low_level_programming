11406ca78105afade02bf9e783c793ceb56a803c
Palesa Mogale <pmogale88@gmail.com>
2022-09-14 04:22:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  *
- * Return: 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the codes for Holberton School students.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
