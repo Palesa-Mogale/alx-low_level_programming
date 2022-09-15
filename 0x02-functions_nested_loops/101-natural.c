d7ac1affedd8fc94d0d7446ff41b88c7729a6a9a
Palesa Mogale <pmogale88@gmail.com>
2022-09-14 21:00:08 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,12 +11,12 @@ int main(void)
 	int i = 1;
 	int total = 0;
 
-	while ( i < 1024)
+	while (i < 1024)
 	{
 		if (i % 3 == 0)
 			total += i;
-		else if (i% 5 == 0)
-			total +=i;
+		else if (i % 5 == 0)
+			total += i;
 		i++;
 	}
 	printf("%d\n", total);
