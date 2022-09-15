cd6d6bc92052267ba2efe6e566cdf16b2d7ad67d
Palesa Mogale <pmogale88@gmail.com>
2022-09-15 13:42:23 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print excludes numbers 2 and 4
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		_putchar(x);
+	}
+	_putchar(10);
+}
