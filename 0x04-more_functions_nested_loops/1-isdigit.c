8a5f969de0f53bbabe7a6af6819aa6bebff4c553
Palesa Mogale <pmogale88@gmail.com>
2022-09-15 16:37:49 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,12 @@
  * @x: number to be checked
  * Return: 1 if it is, 0 otherwise
  */
-int _isdigit(int c)
+int _isdigit(int c) 
 {
 	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
+
 	return (0);
 }
