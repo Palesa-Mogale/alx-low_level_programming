d7cf34d2061a3df138c37f96161dd82d5086e350
Palesa Mogale <pmogale88@gmail.com>
2022-09-15 17:42:46 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - prints the largest prime factor
+ * of a number
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long number = 612852475143;
+	int hash;
+
+	while (hash++ < number / 2)
+	{
+		if (number % hash == 0)
+		{
+			number /= 2;
+			continue;
+		}
+		for (hash = 3; hash < number / 2; hash += 2)
+		{
+			if (number % hash == 0)
+				number /= hash;
+		}
+	}
+	printf("%ld\n", number);
+	return (0);
+}
