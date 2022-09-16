e2e0ceab6cab15a2a2747abde7878406c64cffe4
GitAfrica <pmogale88@gmail.com>
2022-09-16 15:50:29 -0700

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - returns the largest of 3 numbers
+ * @a: integer 1
+ * @b: integer 2
+ * @c: integer 3
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
