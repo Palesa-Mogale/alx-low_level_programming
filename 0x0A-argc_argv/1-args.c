3c6ed38b304c9002705831a243b70610d80cb080
GitAfrica <pmogale88@gmail.com>
2022-09-30 09:19:42 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a number of arguments passed into it
+ * @args: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int args, *char argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
