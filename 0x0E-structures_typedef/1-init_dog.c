b3be99ecc4864b09b0f5edf05279e1ec2fe7c8d9
GitAfrica <pmogale88@gmail.com>
2022-10-15 04:35:25 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Return: 0
+ * Return: no return
  */
 void init_dog(struct dog *d, char name, float age, char *owner)
 {
